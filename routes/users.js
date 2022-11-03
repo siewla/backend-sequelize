684d7342fe974dd6c4fa674fe88efba48a30682d
siewla <siewlapang@gmail.com>
2022-11-03 15:43:07 +0800

    add cookie to cors and session

--- routes/users.js
+++ routes/users.js
@@ -36,13 +36,7 @@ router.post("/login", async (req, res) => {
       return;
     }
     // req.session.email = user.email;
-    res
-      .cookie("token", "asdfghjkl", {
-        sameSite: "none",
-        secure: true,
-      })
-      .status(200)
-      .json({ message: "user is authenciated" });
+    res.cookie("token", "asdfghjkl").status(200).json({ message: "user is authenciated" });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
