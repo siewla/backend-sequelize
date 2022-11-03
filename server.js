e0d708f85b5e95ff14c57acebf41c2b431e306e4
siewla <siewlapang@gmail.com>
2022-11-03 13:51:51 +0800

    update cors

--- server.js
+++ server.js
@@ -8,24 +8,17 @@ const PORT = process.env.PORT || 4000;
 const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: true,
-//     saveUninitialized: true,
-//   })
-// );
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-});
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use(
   cors({
     origin: process.env.FRONT_END_URL || "http://localhost:3000",
-    credentials: true,
-    exposedHeaders: ["set-cookie"],
   })
 );
 
@@ -35,16 +28,6 @@ app.use(express.json());
 // for parsing application/x-www-form-urlencoded
 // extended: true precises that the req.body object will contain values of any type instead of just strings.
 app.use(express.urlencoded({ extended: true }));
-
-// app.use("/", (req, res, next) => {
-//   res.cookie("token", "just to set token in cookie");
-//   // res.send("Cookie Set");
-//   next();
-// });
-
-// app.get("/", (req, res) => {
-//   console.log("Cookie SET");
-// });
 
 app.use("/users", users);
 
